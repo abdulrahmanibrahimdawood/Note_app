e7b8cdc1ede41672706e3099d78eb700a42c051c
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2024-09-28 14:02:39 +0300

    show modalbuttom sheet

--- lib/widgets/note_list_view.dart
+++ lib/widgets/note_list_view.dart
@@ -6,11 +6,16 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return const Padding(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        child: CustomNoteItem(),
-      );
-    });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: CustomNoteItem(),
+            );
+          }),
+    );
   }
 }
